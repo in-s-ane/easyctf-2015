4ba58fd470415294fe9b27a0eb2f7de452d534ab
James Wang <jameswang9909@hotmail.com>
2015-11-04 02:12:05 -0500

    Solve Can You Even

--- Can-You-Even_40/solver.py
+++ Can-You-Even_40/solver.py
@@ -1,7 +1,7 @@
 l = open("can-you-even.in").read().strip()
 L = eval("[" + l + "]")
 f = open("can-you-even.out", "w")
-out = str(filter(lambda x: x%2 == 0, L))[1:-1]
-out = ''.join(out.split())
-f.write(out)
+out = filter(lambda x: x%2 == 0, L)
+f.write("%s\n" % len(out))
 
+# easyctf{?v=8ruJBKFrRCk}
