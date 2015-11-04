f050d0cb76ef31085bd6e3eb8835e4617ae38feb
James Wang <jameswang9909@hotmail.com>
2015-11-04 09:56:08 -0500

    Update qr.py

--- Coordinates_150/qr.py
+++ Coordinates_150/qr.py
@@ -1,13 +1,28 @@
 import Image
 import ImageDraw
+from PIL import Image
 
-img = Image.new("RGB", (50,50), "white")
-draw = ImageDraw.Draw(img)
+img = Image.new("RGB", (30,30), "black")
+# draw = ImageDraw.Draw(img)
 
-coords = eval(open("qr.txt", "r").read())
-dotSize = 1
+# dotSize = 1
 
-for (x,y) in coords:
-    draw.rectangle([x,y,x+dotSize-1,y+dotSize-1], fill="black")
+# for (x,y) in coords:
+#     draw.ellipse([x,y,x+dotSize-1,y+dotSize-1], fill="black")
 
 img.save("qr.png")
+
+WHITE = (255, 255, 255, 255)
+BLACK = (0, 0, 0, 255)
+RED = (255, 0, 0, 0)
+
+coords = eval(open("qr.txt", "r").read())
+def read(img):
+    image = Image.open(img)
+    pixels = image.load()
+    for (x, y) in coords:
+        pixels[x, y] = WHITE
+
+    image.save('out.png')
+
+read("qr.png")
