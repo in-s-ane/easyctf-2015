0003b58a1b5da248d2c812ff9275d0278f8c7f65
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2015-11-04 00:34:50 -0500

    oops, can-you-even

--- Can-You-Even_40/solver.py
+++ Can-You-Even_40/solver.py
@@ -1,5 +1,7 @@
 l = open("can-you-even.in").read().strip()
 L = eval("[" + l + "]")
 f = open("can-you-even.out", "w")
-f.write(str(filter(lambda x: x%2==0, L)))
+out = str(filter(lambda x: x%2 == 0, L))[1:-1]
+out = ''.join(out.split())
+f.write(out)
 
