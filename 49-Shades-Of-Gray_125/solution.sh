1a3bd658047b940236b5df9a6791e55e29e1fc18
elc1798 <subliminalmau5@gmail.com>
2015-11-03 23:24:45 -0500

    idk

---
+++ 49-Shades-Of-Gray_125/solution.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+convert shades.jpg -format %c -depth 8 histogram:info:- | grep -Po '(?<=\().*?(?=\))' > VALUES.txt
+
+# Above command gives us a histogram of all the pixel values.
+
+tr -d ' \t\f' < VALUES.txt > STRIPPED.txt
+
+# Strips the spacesfor easy parsing
+
+for i in `seq 0 255`; do
+    if ! cat STRIPPED.txt | grep $i > /dev/null; then
+        echo $i
+    fi
+done;
+
