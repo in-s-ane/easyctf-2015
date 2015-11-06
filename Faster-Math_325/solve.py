6452b12d6591cb1e637d2da6e8314e3524896f55
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2015-11-05 21:12:21 -0500

    loopy loop loop

--- Faster-Math_325/solve.py
+++ Faster-Math_325/solve.py
@@ -31,97 +31,101 @@ def make_change(amt, S):
 HOST = 'programming.easyctf.com'
 PORT = 10300
 
-def answer(s=None):
-    if not s:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST,PORT))
-    time.sleep(0.5)
-    prob = s.recv(1024)
-    print prob
-    if "flag" in prob or "{" in prob:
+def answer():
+    s = None
+    while True:
+        if not s:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST,PORT))
+        time.sleep(0.5)
+        prob = s.recv(1024)
         print prob
-    if "seconds" in prob:
-        print prob.split("seconds")[0].split()[-1]
-    if "Sorry, incorrect" in prob:
-        print prob
-        s.close()
-        answer()
-    if "Find the value of the lesser root" in prob:
-        halves = prob.split("x^2")
-        a = int(halves[0].split()[-1])
-        halves2 = halves[1].split("x")
-        b = int(halves2[0].split()[-1])
-        c = int(halves2[1].split()[1])
+        if "flag" in prob or "{" in prob:
+            print prob
+        if "seconds" in prob:
+            print prob.split("seconds")[0].split()[-1]
+        if "Sorry, incorrect" in prob:
+            print prob
+            s.close()
+            s = None
+            continue
+        if "Find the value of the lesser root" in prob:
+            halves = prob.split("x^2")
+            a = int(halves[0].split()[-1])
+            halves2 = halves[1].split("x")
+            b = int(halves2[0].split()[-1])
+            c = int(halves2[1].split()[1])
 
-        if halves2[0].split()[0] == "-":
-            b = -b
-        if halves2[1].split()[0] == "-":
-            c = -c
-        s.sendall(str(int(lesser_root(a,b,c))))
-    elif "Find the value of the greater root" in prob:
-        halves = prob.split("x^2")
-        a = int(halves[0].split()[-1])
-        halves2 = halves[1].split("x")
-        b = int(halves2[0].split()[-1])
-        c = int(halves2[1].split()[1])
+            if halves2[0].split()[0] == "-":
+                b = -b
+            if halves2[1].split()[0] == "-":
+                c = -c
+            s.sendall(str(int(lesser_root(a,b,c))))
+        elif "Find the value of the greater root" in prob:
+            halves = prob.split("x^2")
+            a = int(halves[0].split()[-1])
+            halves2 = halves[1].split("x")
+            b = int(halves2[0].split()[-1])
+            c = int(halves2[1].split()[1])
 
-        if halves2[0].split()[0] == "-":
-            b = -b
-        if halves2[1].split()[0] == "-":
-            c = -c
-        s.sendall(str(int(greater_root(a,b,c))))
-    elif "Fill the operations" in prob:
-        prob = [x for x in prob.splitlines() if "Fill" in x][0]
-        s.sendall(fill_operators(prob))
-    elif "first touches the floor" in prob:
-        halves = prob.split("m/s")
-        v = int(halves[0].split()[-1])
-        halves2 = halves[1].split(" m ")
-        d = int(halves2[0].split()[-1])
-        halves3 = halves2[1].split(" g ")
-        w = int(halves3[0].split()[-1])
-        halves4 = halves3[1].split(" m/s^2 ")
-        a = int(halves4[0].split()[-1])
-        s.sendall(str(int(math.sqrt(2*d/a)*v)))
-    elif "is projected" in prob:
-        halves = prob.split("m/s")
-        v = int(halves[0].split()[-1])
-        halves2 = halves[1].split(" m ")
-        d = int(halves2[0].split()[-1])
-        halves3 = halves2[1].split(" g ")
-        w = int(halves3[0].split()[-1])
-        halves4 = halves3[1].split(" m/s^2 ")
-        a = int(halves4[0].split()[-1])
-        s.sendall(str(int(math.sqrt(2*a*d))))
-    elif "I was going to" in prob:
-        S = []
-        if "pennies" in prob:
-            S += [1]
-        if "nickels" in prob:
-            S += [5]
-        if "dimes" in prob:
-            S += [10]
-        if "quarters" in prob:
-            S += [25]
-        if " dollar" in prob:
-            S += [100]
-        if "five-dollar" in prob:
-            S += [500]
-        if "ten-dollar" in prob:
-            S += [1000]
-        amt = prob.split("$")[1].split()[0]
-        dollars = int(amt.split(".")[0])
-        cents = int(amt.split(".")[1])
-        if len(amt.split(".")[1]) == 1: cents *= 10
-        print "Change poss:", make_change(dollars*100 + cents, S)
-        s.sendall(str(make_change(dollars*100 + cents, S)))
-    else:
-        print "Couldn't solve:"
-        print prob
-        s.close()
-        answer()
-
-    answer(s)
+            if halves2[0].split()[0] == "-":
+                b = -b
+            if halves2[1].split()[0] == "-":
+                c = -c
+            s.sendall(str(int(greater_root(a,b,c))))
+        elif "Fill the operations" in prob:
+            prob = [x for x in prob.splitlines() if "Fill" in x][0]
+            s.sendall(fill_operators(prob))
+        elif "first touches the floor" in prob:
+            halves = prob.split("m/s")
+            v = int(halves[0].split()[-1])
+            halves2 = halves[1].split(" m ")
+            d = int(halves2[0].split()[-1])
+            halves3 = halves2[1].split(" g ")
+            w = int(halves3[0].split()[-1])
+            halves4 = halves3[1].split(" m/s^2 ")
+            a = int(halves4[0].split()[-1])
+            s.sendall(str(int(math.sqrt(2*d/a)*v)))
+        elif "is projected" in prob:
+            halves = prob.split("m/s")
+            v = int(halves[0].split()[-1])
+            halves2 = halves[1].split(" m ")
+            d = int(halves2[0].split()[-1])
+            halves3 = halves2[1].split(" g ")
+            w = int(halves3[0].split()[-1])
+            halves4 = halves3[1].split(" m/s^2 ")
+            a = int(halves4[0].split()[-1])
+            s.sendall(str(int(math.sqrt(2*a*d))))
+        elif "I was going to" in prob:
+            S = []
+            if "pennies" in prob:
+                S += [1]
+            if "nickels" in prob:
+                S += [5]
+            if "dimes" in prob:
+                S += [10]
+            if "quarters" in prob:
+                S += [25]
+            if " dollar" in prob:
+                S += [100]
+            if "five-dollar" in prob:
+                S += [500]
+            if "ten-dollar" in prob:
+                S += [1000]
+            amt = prob.split("$")[1].split()[0]
+            dollars = int(amt.split(".")[0])
+            cents = int(amt.split(".")[1])
+            if len(amt.split(".")[1]) == 1: cents *= 10
+            print "Change poss:", make_change(dollars*100 + cents, S)
+            s.sendall(str(make_change(dollars*100 + cents, S)))
+        else:
+            if "Solve this problem" in prob:
+                continue
+            print "Couldn't solve:"
+            print prob
+            s.close()
+            s = None
+            continue
 
 if __name__ == '__main__':
     answer()
